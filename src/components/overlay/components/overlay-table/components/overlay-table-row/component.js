c9aa1719f637bb8eab9ba3f9c1e490cb282c44c6
Ch. König <criss@paars.de>
2017-11-14 16:51:20 +0100

    render background maybe on next tick

--- src/components/overlay/components/overlay-table/components/overlay-table-row/component.js
+++ src/components/overlay/components/overlay-table/components/overlay-table-row/component.js
@@ -15,13 +15,15 @@ module.exports = class {
   }
   onRender () {
     if (!this.mounted) return
-    const row = this.getEl('trrow')
-    const canvas = this.getEl('canvas')
-    canvas.width = row.clientWidth
-    canvas.height = row.clientHeight
-    const ctx = canvas.getContext('2d')
-    meter.render(ctx, this.input.combatant, JSON.parse(localStorage.getItem('meter')))
-    row.style.backgroundImage = 'url(' + canvas.toDataURL() + ')'
+    setTimeout(() => {
+      const row = this.getEl('trrow')
+      const canvas = this.getEl('canvas')
+      canvas.width = row.clientWidth
+      canvas.height = row.clientHeight
+      const ctx = canvas.getContext('2d')
+      meter.render(ctx, this.input.combatant, JSON.parse(localStorage.getItem('meter')))
+      row.style.backgroundImage = 'url(' + canvas.toDataURL() + ')'
+    }, 1)
   }
 
   onResize () {
